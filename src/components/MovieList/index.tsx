2d70885fae43ce12bc3033b7ceb32cb4d8b64ade
GuilhermeEduardo23 <guilhermeeduardo-17@outlook.com>
2025-01-16 16:25:44 +0000

    Adding Loading before refresh the page

--- src/components/MovieList/index.tsx
+++ src/components/MovieList/index.tsx
@@ -3,7 +3,6 @@ import "./index.scss";
 import axios from "axios";
 import MovieCard from "../MovieCard";
 import { Movie } from "@/Types/movie";
-import ReactLoading from "react-loading";
 
 const MovieList = () => {
     const [movies, setMovies] = useState<Movie[]>([]);
@@ -29,16 +28,18 @@ const MovieList = () => {
     }, []);
     
     if(isLoading) {
-        
+        return <div className="loading-container">
+            <h1>Carregando...</h1>
+        </div>
+    } else {
+        return (
+            <ul className="movie-list">
+                {movies.map((movie) => (
+                    <MovieCard key={movie.id} movie={movie} />
+                ))}
+            </ul>
+        )
     }
-
-    return (
-        <ul className="movie-list">
-            {movies.map((movie) => (
-                <MovieCard key={movie.id} movie={movie} />
-            ))}
-        </ul>
-    )
 }
 
 export default MovieList;
